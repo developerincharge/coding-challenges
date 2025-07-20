8ae7b3e02783e00ce1831a2c23682b21807e686e
sabur786 <imtiazrizvi786@yahoo.com>
2025-07-20 01:10:28 -0500

    more coding challenges

--- src/main/java/com/rizvi/stream/LengthOfStringElements.java
+++ src/main/java/com/rizvi/stream/LengthOfStringElements.java
@@ -1,13 +1,16 @@
 package com.rizvi.stream;
 
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class LengthOfStringElements {
 
     public static void main(String[] args) {
 
-        List<String> fruits = Arrays.asList("apple", "banana", "papaya","hyacinth","cherry", "date","guava", "fig", "elderberry", "jackfruit", "lemon", "pineapple","kiwi","strawberry", "banana", "date","fig");
+        List<String> fruits = Arrays.asList("apple", "banana", "strawberry","papaya","hyacinth","cherry", "date","guava", "fig", "elderberry", "jackfruit", "lemon", "pineapple","kiwi","strawberry", "banana", "date","fig");
 
         //fruits.stream().map(String::length).forEach(System.out::println);
 
@@ -18,5 +21,63 @@ public class LengthOfStringElements {
 //        for (String fruit : fruits) {
 //            System.out.println(fruit + " -> " + fruit.length());
 //        }
+
+        System.out.println("#######################");
+        fruits.stream()
+                .distinct()
+                .sorted((o1,o2) -> o1.length() - o2.length())
+                .forEach(System.out::println);
+
+        System.out.println("#########################");
+
+        fruits.stream()
+                .sorted(Comparator.comparing(String::length).reversed())
+                .forEach(System.out::println);
+
+        System.out.println("##########################");
+
+        fruits.stream()
+                .max(Comparator.comparing(String::length))
+                .ifPresent(System.out::println);
+
+        System.out.println("#######################");
+
+        fruits.stream()
+                .filter(str -> str.matches(".*[aeiou].*"))
+                .forEach(System.out::println);
+
+
+        System.out.println("#######################");
+
+          String fruitList =  fruits.stream()
+                   .collect(Collectors.joining(","));
+          System.out.println(fruitList);
+
+        System.out.println("#######################");
+
+        fruits.stream()
+                .map(str -> new StringBuilder(str).reverse())
+                .forEach(System.out::println);
+
+        System.out.println("#########################");
+
+        Map<Character, List<String>> firstCharMap =  fruits.stream()
+                .collect(Collectors.groupingBy(str -> str.charAt(0)));
+        firstCharMap.forEach((k, v) -> System.out.println(k + " -> " + v));
+
+        System.out.println("#########################");
+
+        Map<Character, List<String>> lastCharMap =  fruits.stream()
+                .collect(Collectors.groupingBy(str -> str.charAt(str.length() -1)));
+        lastCharMap.forEach((k, v) -> System.out.println(k + " -> " + v));
+
+
+
+        System.out.println("#########################");
+
+        Map<Integer, List<String>> lengthMap =  fruits.stream().distinct()
+                .collect(Collectors.groupingBy(String::length));
+        lengthMap.forEach((k, v) -> System.out.println(k + " -> " + v));
+
     }
 }
